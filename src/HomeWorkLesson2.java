f4368c1285d40824bf5b139d3d17386e70598afa
Sergey <sergey@MacBook-Pro-Sergey.local>
2023-11-10 17:03:00 +0100

    Initial commit

---
+++ src/HomeWorkLesson2.java
@@ -0,0 +1,15 @@
+public class HomeWorkLesson2 {
+
+        public static void main(String[] args) {
+
+            int x = 369;
+            int e = 369 % 10;
+            int t = ( x % 369) / 10;
+            int s = 369 /100;
+            System.out.println( e );
+            System.out.println( t );
+            System.out.println( s );
+
+        }
+    }
+
